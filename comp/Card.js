f599c07186c186c3c0df9088cac77d7c4229663e
SueLee <slee1008@my.bcit.ca>
2023-01-24 13:30:12 -0800

    set default for book & remove conditional for result

--- comp/Card.js
+++ comp/Card.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react"
 import Modal from "./Modal";
 
-export default function Card({book}){
+export default function Card({book = []}){
 
     const [show, setShow] = useState(false);
     const [bookItem, setBookItem] = useState();
-
+    // console.log(book)
     return(
         <>
             {
                 book.map((item, index)=>{
                     let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
                     let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
-                    if(thumbnail != undefined && amount != undefined){
+                    
                         return(
-                        <>
-                            <div className="card" key={index} onClick={()=>{ setShow(true); setBookItem(item)}}>
+                        <div key = {index}>
+                            <div className="card"  onClick={()=>{ setShow(true); setBookItem(item)}}>
                                 <img src={thumbnail} alt="Harry Potter" />
                                 <div className="bottom">
                                     <h3 className="title">{item.volumeInfo.title}</h3>
-                                    <p className="amount">&#36;{amount}</p>
+                                    {/* <p className="amount">&#36;{amount}</p> */}
                                 </div>
                             </div>
                             <Modal show={show} item={bookItem} onClose={()=>setShow(false)} /> 
-                        </>
+                        </div>
                         )    
-                    }
+                    
                     
                 })
             }
