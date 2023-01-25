1dc68fea6fadd8f927e37579721b0dbd4a0ee85d
seohyun9672 <ajeong4@my.bcit.ca>
2023-01-24 21:08:16 -0800

    fix layout

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,21 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import axios from 'axios'
-import React, { useState } from 'react'
-import Card from '@/comp/Card'
+import { Inter } from "@next/font/google";
+import React from "react";
+import { useRouter } from "next/router";
+import Button from "@/comp/Button";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
-    const [search, setSearch] = useState("")
-    const [bookData, setBookData] = useState([]);
-    const searchBook=(e)=>{
-        if(e.key === "Enter"){
-            axios.get('https://www.googleapis.com/books/v1/volumes?q=subject:'+search+'&key=AIzaSyAILo6V3o56v_azR9rm8GZVzrBMgLogWDQ&maxResults=40')
-            .then(res=>{
-            setBookData(res.data.items)
-            }).catch(err=>{
-            console.log(err)
-            })
-        }
-    }
+  const router = useRouter();
 
-    return(
-        <>
-            <div className= 'header'>
-                <div className="row1">
-                    <h1>Welcome to Bookie</h1>
-                </div>
-                <div className="row2">
-                    <h2>Find your book</h2>
-                    <div className="search">
-                        <input 
-                        type="text" 
-                        placeholder="Enter Genre of Book" 
-                        value={search}
-                        onChange={e=>setSearch(e.target.value)}
-                        onKeyDown={searchBook}
-                        />
-                        <button
-                            onClick={searchBook}
-                        >Search</button>
-                    </div>
-                </div>
-
-            </div>
-
-            <div className="container">
-                {
-                    <Card book={bookData} />
-                }
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="header">
+        <h1>Welcome to Bookie</h1>
+        <h2>Find a book you like.</h2>
+      </div>
+      <Button text="Get started" handleClick={() => router.push("/onboarding")} />
+    </>
+  );
+};
