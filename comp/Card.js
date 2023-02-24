b6826084bb88d84aed2f4858b657374c2e9b224e
seohyun9672 <ajeong4@my.bcit.ca>
2023-02-24 12:55:16 -0800

    remove map inside the component

--- comp/Card.js
+++ comp/Card.js
@@ -1,35 +1,52 @@
 import React, { useState } from "react"
-import Modal from "./Modal";
+import styled from "styled-components";
 
-export default function Card({ book = [] }) {
+const CardCont = styled.div`
+box-sizing: border-box;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+width: 258px;
+height: 335px;
+border: 3.10748px solid var(--text-main-color);
+border-radius: 15px;
+`
 
-  const [show, setShow] = useState(false);
-  const [bookItem, setBookItem] = useState();
-  // console.log(book)
+export default function Card({ title = "", author = "", src = "" }) {
+
   return (
-    <>
-      {
-        book.map((item, index) => {
-          // let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-          let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+    <CardCont>
+      <div>
+        <img src={src} />
+      </div>
+      <div>{title}</div>
+      <div>{author}</div>
+    </CardCont>
 
-          return (
-            <div key={index}>
-              <div className="card" onClick={() => { setShow(true); setBookItem(item) }}>
-                <img src={thumbnail} alt="Harry Potter" />
-                <div className="bottom">
-                  <h3 className="title">{item.volumeInfo.title}</h3>
-                  {/* <p className="amount">&#36;{amount}</p> */}
-                </div>
-              </div>
-              <Modal show={show} item={bookItem} onClose={() => setShow(false)} />
-            </div>
-          )
+    // <>
+    //   {
+    //     book.map((item, index) => {
+    //       // let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+    //       let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+
+    //       return (
+    //         <div key={index}>
+    //           <div className="card" onClick={() => { setShow(true); setBookItem(item) }}>
+    //             <img src={thumbnail} alt="Harry Potter" />
+    //             <div className="bottom">
+    //               <h3 className="title">{item.volumeInfo.title}</h3>
+    //               {/* <p className="amount">&#36;{amount}</p> */}
+    //             </div>
+    //           </div>
+    //           <Modal show={show} item={bookItem} onClose={() => setShow(false)} />
+    //         </div>
+    //       )
 
 
-        })
-      }
+    //     })
+    //   }
 
-    </>
+    // </>
   )
 }
