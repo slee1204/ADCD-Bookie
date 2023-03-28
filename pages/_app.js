b38cadecfbdece670afc4ae13b865c757a644ed9
SueLee <slee1008@my.bcit.ca>
2023-03-28 16:15:27 -0700

    set GlobalStyle for overflow

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import Navbar from '@/comp/Navbar'
 import '@/styles/globals.css'
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    overflow: auto;
+  }
+`;
 
 export default function App({ Component, pageProps }) {
   return <>
+    <GlobalStyle />
     <Navbar />
     <Component {...pageProps} />
   </>
